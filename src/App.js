235c086d499ed2c00f213e49d6ac3a33a73d208c
J AY <javi.ayanez@gmail.com>
2025-06-19 13:51:46 +0200

    cambios para enrutado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import TableroCasual from './components/Tablero/Casual/TableroCasual';
 import TableroCompetitivo from './components/Tablero/Competitivo/TableroCompetitivo';
 import Ranking from './components/Tablero/RankingCompetitivo';
@@ -7,44 +7,57 @@ function App() {
   return (
     <Router>
       <div className="App">
-         
-          <nav className="nav">
-              <Link to="/casual"><button style={{ 
-                    marginRight: 10, 
-                    marginTop: 10,
-                    padding: '8px 16px', 
-                    cursor: 'pointer', 
-                    backgroundColor: '#5A5A5A',
-                    color:'#FAF9F7', 
-                    fontWeight: 'bold',
-                    borderRadius: '15px' }}
-                >Tablero Casual</button></Link>
-              <Link to="/competitivo"><button style={{ 
-                    marginRight: 10,
-                    marginTop: 10, 
-                    padding: '8px 16px', 
-                    cursor: 'pointer', 
-                    backgroundColor: '#5A5A5A',
-                    color:'#FAF9F7', 
-                    fontWeight: 'bold',
-                borderRadius: '15px' }}>Tablero Competitivo</button></Link>
-              <Link to="/ranking"><button style={{ 
-                   marginRight: 10,
-                   marginTop: 10, 
-                    padding: '8px 16px', 
-                    cursor: 'pointer', 
-                    backgroundColor: '#5A5A5A',
-                    color:'#FAF9F7', 
-                    fontWeight: 'bold',
-                    borderRadius: '15px' }}>Ranking Competitivo</button></Link>
-           
-          </nav>
+        <nav className="nav">
+          <Link to="/casual">
+            <button style={{ 
+              marginRight: 10, 
+              marginTop: 10,
+              padding: '8px 16px', 
+              cursor: 'pointer', 
+              backgroundColor: '#5A5A5A',
+              color:'#FAF9F7', 
+              fontWeight: 'bold',
+              borderRadius: '15px' 
+            }}>
+              Tablero Casual
+            </button>
+          </Link>
+          <Link to="/competitivo">
+            <button style={{ 
+              marginRight: 10,
+              marginTop: 10, 
+              padding: '8px 16px', 
+              cursor: 'pointer', 
+              backgroundColor: '#5A5A5A',
+              color:'#FAF9F7', 
+              fontWeight: 'bold',
+              borderRadius: '15px' 
+            }}>
+              Tablero Competitivo
+            </button>
+          </Link>
+          <Link to="/ranking">
+            <button style={{ 
+              marginRight: 10,
+              marginTop: 10, 
+              padding: '8px 16px', 
+              cursor: 'pointer', 
+              backgroundColor: '#5A5A5A',
+              color:'#FAF9F7', 
+              fontWeight: 'bold',
+              borderRadius: '15px' 
+            }}>
+              Ranking Competitivo
+            </button>
+          </Link>
+        </nav>
 
         <Routes>
+          <Route path="/" element={<Navigate to="/casual" replace />} />
           <Route path="/casual" element={<TableroCasual />} />
           <Route path="/competitivo" element={<TableroCompetitivo />} />
           <Route path="/ranking" element={<Ranking />} />
-          <Route path="*"  element={<TableroCasual />} />
+          <Route path="*" element={<Navigate to="/casual" replace />} />
         </Routes>
       </div>
     </Router>
